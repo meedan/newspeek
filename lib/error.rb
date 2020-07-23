d6a31e4e02f1f824780d8691670e3324f947ead0
Devin Gaffney <itsme@devingaffney.com>
2020-07-23 09:33:13 -0400

    Also don't raise in testing environment

--- lib/error.rb
+++ lib/error.rb
@@ -1,6 +1,7 @@
 class Error
   def self.log(exception, opts={})
-    Airbrake.notify(exception, opts) unless Settings.blank?('airbrake_api_host')
+    Airbrake.notify(exception, opts) unless Settings.airbrake_specified?
+    raise exception if Settings.airbrake_unspecified?
     nil
   end
 end
