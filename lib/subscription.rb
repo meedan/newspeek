032eb8d85830cd7bf59026e7dba5c3ae013d0473
Devin Gaffney <itsme@devingaffney.com>
2020-07-27 14:48:22 -0400

    updates per discussion on ticket

--- lib/subscription.rb
+++ lib/subscription.rb
@@ -3,20 +3,30 @@ class Subscription
     "claim_review_webhooks_#{service}"
   end
 
-  def self.add_subscription(service, url)
-    REDIS_CLIENT.sadd(self.keyname(service), url)
+  def self.add_subscription(services, urls)
+    [services].flatten.collect do |service|
+      [urls].flatten.collect do |url|
+        REDIS_CLIENT.sadd(self.keyname(service), url)
+      end
+    end.flatten
   end
   
-  def self.remove_subscription(service, url)
-    REDIS_CLIENT.srem(self.keyname(service), url)
+  def self.remove_subscription(services, urls)
+    [services].flatten.collect do |service|
+      [urls].flatten.collect do |url|
+        REDIS_CLIENT.srem(self.keyname(service), url)
+      end
+    end.flatten
   end
 
-  def self.get_subscriptions(service)
-    REDIS_CLIENT.smembers(self.keyname(service)) || []
+  def self.get_subscriptions(services)
+    [services].flatten.collect do |service|
+      REDIS_CLIENT.smembers(self.keyname(service)) || []
+    end.flatten
   end
 
-  def self.notify_subscribers(service, claim_review)
-    self.get_subscriptions(service).each do |webhook_url|
+  def self.notify_subscribers(services, claim_review)
+    self.get_subscriptions(services).each do |webhook_url|
       RestClient.post(webhook_url, {claim_review: claim_review}.to_json)
     end
   end
