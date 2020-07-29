57f11df607bc4accbe7f129af1059035d7fee80d
Aaron Huslage <aaron@meedan.com>
2020-07-29 13:38:44 -0400

    feature/deploy (#18)
    * Run configurator at startup

---
+++ production/bin/configurator.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+if [[ -z ${GITHUB_TOKEN+x} || -z ${DEPLOY_ENV+x} || -z ${APP+x} ]]; then
+	echo "GITHUB_TOKEN, DEPLOY_ENV and APP must be in the environment.   Exiting."
+	exit 1
+fi
+
+if [ ! -d "configurator" ]; then git clone -q https://${GITHUB_TOKEN}:x-oauth-basic@github.com/meedan/configurator ./configurator; fi
+d=configurator/check/${DEPLOY_ENV}/${APP}/; for f in $(find $d -type f); do cp "$f" "${f/$d/}"; done
+
