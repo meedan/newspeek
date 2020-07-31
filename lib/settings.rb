22a04867b31add5039abe0e30e72faf9828cb7d8
Devin Gaffney <itsme@devingaffney.com>
2020-07-31 10:35:57 -0400

    Change ES checkin to happen in ruby instead of Makefile h/t Adam Hooper for his article about Ruby Timeout code

--- lib/settings.rb
+++ lib/settings.rb
@@ -23,10 +23,45 @@ class Settings
       'redis_host' => 'redis',
       'redis_port' => 6379,
       'redis_database' => 1,
+      'env' => 'test',
     }
   end
 
   def self.redis_url
     "redis://#{Settings.get('redis_host')}:#{Settings.get('redis_port')}/#{Settings.get('redis_database')}"
+  end
+
+  def self.in_test_mode?
+    Settings.get('env') != 'test'
+  end
+
+  def self.attempt_elasticsearch_connect(url)
+    Net::HTTP.start(
+      url.host,
+      url.port,
+      use_ssl: url.scheme == 'https',
+      open_timeout: 5,
+      read_timeout: 5,
+      ssl_timeout: 5
+    ) { |http| http.request(Net::HTTP::Get.new(url)) }
+  end
+
+  def self.check_into_elasticsearch(timeout=60, bypass=!Settings.in_test_mode?)
+    unless bypass
+      start = Time.now
+      url = URI.parse(Settings.get('es_host'))
+      res = nil
+      begin
+        res = Settings.attempt_elasticsearch_connect(url)
+      rescue Errno::ECONNREFUSED, SocketError
+        sleep(1)
+        retry if start+timeout > Time.now
+      end
+      raise Settings.elastic_search_error if res.nil?
+    end
+  end
+
+  def self.elastic_search_error
+    StandardError.new("Could not connect to elasticsearch host located at #{Settings.get('es_host')}!")
   end
 end
--- spec/lib/settings_test.rb
+++ spec/lib/settings_test.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 describe Settings do
+  before do
+    stub_request(:any, 'http://elasticsearch:9200/').to_raise(SocketError)
+  end
   describe 'class methods' do
     it 'has allows get access' do
       expect(Settings.get('blah')).to(eq(nil))
@@ -13,6 +16,9 @@ describe Settings do
     it 'has defaults' do
       expect(Settings.defaults.class).to(eq(Hash))
     end
-
+    
+    it 'fires timeout on failed es connect' do
+      expect{Settings.check_into_elasticsearch(1, false)}.to(raise_error(StandardError))
+    end
   end
 end
