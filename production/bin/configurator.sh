ca5748f71a33c7da415d6d92248215860fbe2a88
infojunkie <karim.ratib@gmail.com>
2020-08-25 13:46:03 -0700

    Pass env to docker and restore GITHUB_TOKEN

--- production/bin/configurator.sh
+++ production/bin/configurator.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-if [[ -z ${CONFIGURATOR_TOKEN+x} || -z ${DEPLOY_ENV+x} || -z ${APP+x} ]]; then
-	echo "CONFIGURATOR_TOKEN, DEPLOY_ENV and APP must be in the environment.   Exiting."
+if [[ -z ${GITHUB_TOKEN+x} || -z ${DEPLOY_ENV+x} || -z ${APP+x} ]]; then
+	echo "GITHUB_TOKEN, DEPLOY_ENV and APP must be in the environment.   Exiting."
 	exit 1
 fi
 
-if [ ! -d "configurator" ]; then git clone -q https://${CONFIGURATOR_TOKEN}:x-oauth-basic@github.com/meedan/configurator ./configurator; fi
+if [ ! -d "configurator" ]; then git clone -q https://${GITHUB_TOKEN}:x-oauth-basic@github.com/meedan/configurator ./configurator; fi
 d=configurator/check/${DEPLOY_ENV}/${APP}/; for f in $(find $d -type f); do cp "$f" "${f/$d/}"; done
 
