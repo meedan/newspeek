78deb7547364edcb49c5a6ecab1cdcbd92d5d6ef
Devin Gaffney <itsme@devingaffney.com>
2020-08-31 09:18:27 -0400

    Update fixture and update estadao to reflect claimreviews instead

--- lib/claim_review_parsers/estadao.rb
+++ lib/claim_review_parsers/estadao.rb
@@ -26,6 +26,20 @@ class Estadao < ClaimReviewParser
     claim_review["image"]["url"][0]
   end
 
+  def claim_review_result_from_claim_review(claim_review)
+    claim_review &&
+    claim_review["reviewRating"] &&
+    claim_review["reviewRating"]["alternateName"]
+  end
+  
+  def claim_review_headline_from_raw_claim_review(raw_claim_review)
+    raw_claim_review["page"].search("article.n--noticia__header h1.n--noticia__title").text
+  end
+
+  def claim_review_body_from_raw_claim_review(raw_claim_review)
+    raw_claim_review["page"].search("div.n--noticia__content p").text
+  end
+
   def parse_raw_claim_review(raw_claim_review)
     claim_review = extract_ld_json_script_block(raw_claim_review["page"], 0)
     {
@@ -33,12 +47,12 @@ class Estadao < ClaimReviewParser
       created_at: Time.parse(claim_review["datePublished"]),
       author: claim_review["author"]["name"],
       author_link: claim_review["author"]["url"],
-      claim_review_headline: claim_review["headline"],
-      claim_review_body: claim_review["description"],
-      claim_review_reviewed: nil,
+      claim_review_headline: claim_review_headline_from_raw_claim_review(raw_claim_review),
+      claim_review_body: claim_review_body_from_raw_claim_review(raw_claim_review),
+      claim_review_reviewed: claim_review["claimReviewed"],
       claim_review_image_url: claim_review_image_url_from_claim_review(claim_review),
-      claim_review_result: nil,
-      claim_review_result_score: nil,
+      claim_review_result: claim_review_result_from_claim_review(claim_review),
+      claim_review_result_score: claim_result_score_from_raw_claim_review(claim_review),
       claim_review_url: raw_claim_review['url'],
       raw_claim_review: claim_review
     }
