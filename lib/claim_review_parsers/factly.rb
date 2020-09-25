cb30150be6e751724a2333ecdf35c9a331584334
Devin Gaffney <itsme@devingaffney.com>
2020-09-25 12:50:01 -0400

    update factly to work with API

--- lib/claim_review_parsers/factly.rb
+++ lib/claim_review_parsers/factly.rb
@@ -3,20 +3,37 @@
 # Parser for https://factly.in
 class Factly < ClaimReviewParser
   include PaginatedReviewClaims
+  def initialize(cursor_back_to_date = nil, overwrite_existing_claims=false)
+    super(cursor_back_to_date, overwrite_existing_claims)
+    @fact_list_page_parser = 'json'
+  end
+
+  def get_new_fact_page_urls(page)
+    response = get_fact_page_urls(page)
+    existing_urls = get_existing_urls(response.collect{|d| d["link"]})
+    response.select{|d| !existing_urls.include?(d["link"])}
+  end
+
+  def parsed_fact_page(fact_page_response)
+    parsed_page = parsed_page_from_url(fact_page_response["link"])
+    return if parsed_page.nil?
+    [fact_page_response["link"], parse_raw_claim_review(Hashie::Mash[{ raw_response: fact_page_response, page: parsed_page, url: fact_page_response["link"] }])]
+  end
+
   def hostname
     'https://factly.in'
   end
 
   def fact_list_path(page = 1)
-    "/category/fact-check/page/#{page}/"
+    "/wp-json/wp/v2/posts?page=#{page}"
   end
 
-  def url_extraction_search
-    'div.main-content div.column h2.post-title a'
+  def fact_check_categories
+    [357, 305]
   end
 
-  def url_extractor(atag)
-    atag.attributes['href'].value
+  def url_extractor(response)
+    response.select{|r| (r["categories"]&fact_check_categories).length > 0}
   end
 
   def get_fact_index_from_page(page)
@@ -36,16 +53,16 @@ class Factly < ClaimReviewParser
     article = extract_ld_json_script_block(raw_claim_review["page"], -1)
     {
       id: raw_claim_review['url'],
-      created_at: Time.parse(article["datePublished"]),
-      author: article["author"]["name"],
-      author_link: raw_claim_review['page'].search('span.posted-by span.reviewer a').first.attributes['href'].value,
-      claim_review_headline: article["headline"],
-      claim_review_body: raw_claim_review['page'].search('div.post-content p').text,
+      created_at: Time.parse(raw_claim_review['raw_response']['date']),
+      author: raw_claim_review['raw_response']['author_meta']['display_name'],
+      author_link: raw_claim_review['raw_response']['author_meta']['author_link'],
+      claim_review_headline: raw_claim_review['raw_response']['title']['rendered'],
+      claim_review_body: raw_claim_review['raw_response']['content']['rendered'],
       claim_review_image_url: claim_review_image_url_from_raw_claim_review(raw_claim_review),
       claim_review_result: get_claim_result_from_page(raw_claim_review['page']),
       claim_review_result_score: nil,
       claim_review_url: raw_claim_review['url'],
-      raw_claim_review: article
+      raw_claim_review: {article: article, api_response: raw_claim_review['raw_response']}
     }
   end
 end
--- spec/lib/claim_review_parsers/factly_test.rb
+++ spec/lib/claim_review_parsers/factly_test.rb
@@ -1,21 +1,44 @@
 # frozen_string_literal: true
 
 describe Factly do
+  before do
+    stub_request(:get, JSON.parse(File.read('spec/fixtures/factly_raw.json'))["url"])
+      .with(
+        headers: {
+          'Accept' => '*/*',
+          'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+          'Host' => 'factly.in',
+          'User-Agent' => /.*/
+        }
+      )
+      .to_return(status: 200, body: JSON.parse(File.read('spec/fixtures/factly_raw.json'))["page"], headers: {})
+  end
   describe 'instance' do
+    it 'extracts raw responses from get_new_fact_page_urls' do
+      described_class.any_instance.stub(:get_fact_page_urls).with(1).and_return([{'link' => '/blah', 'categories' => [123]}, {'link' => '/blah2', 'categories' => [305]}])
+      described_class.any_instance.stub(:get_existing_urls).with(anything()).and_return([])
+      expect(described_class.new.get_new_fact_page_urls(1)).to(eq([{"categories"=>[123], "link"=>"/blah"}, {'link' => '/blah2', 'categories' => [305]}]))
+    end
+
+    it 'extracts parsed_fact_page results' do
+      keys = [:author, :author_link, :claim_review_body, :claim_review_headline, :claim_review_image_url, :claim_review_result, :claim_review_result_score, :claim_review_url, :created_at, :id, :raw_claim_review].sort
+      raw = JSON.parse(File.read('spec/fixtures/factly_raw.json'))
+      response = described_class.new.parsed_fact_page(raw["raw_response"])
+      expect(response[0]).to(eq(raw['url']))
+      expect(response[1].class).to(eq(Hash))
+      expect(response[1].keys.sort).to(eq(keys))
+    end
+    
     it 'has a hostname' do
       expect(described_class.new.hostname).to(eq('https://factly.in'))
     end
 
     it 'has a fact_list_path' do
-      expect(described_class.new.fact_list_path(1)).to(eq('/category/fact-check/page/1/'))
-    end
-
-    it 'has a url_extraction_search' do
-      expect(described_class.new.url_extraction_search).to(eq('div.main-content div.column h2.post-title a'))
+      expect(described_class.new.fact_list_path(1)).to(eq('/wp-json/wp/v2/posts?page=1'))
     end
 
     it 'extracts a url' do
-      expect(described_class.new.url_extractor(Nokogiri.parse("<a href='/blah'>wow</a>").search('a')[0])).to(eq('/blah'))
+      expect(described_class.new.url_extractor([{'categories' => [123]}, {'categories' => [305]}])).to(eq([{'categories' => [305]}]))
     end
 
     it 'rescues against a claim_review_image_url_from_raw_claim_review' do
