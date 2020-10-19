a741e7089a6ad1f7958b2cd31430e84901d938b4
Devin Gaffney <itsme@devingaffney.com>
2020-10-19 12:20:30 -0400

    Add error logging on failed webhook notifications

--- lib/error.rb
+++ lib/error.rb
@@ -1,7 +1,7 @@
 class Error
-  def self.log(exception, opts={})
+  def self.log(exception, opts={}, should_raise=Settings.airbrake_unspecified?)
     Airbrake.notify(exception, opts) unless Settings.airbrake_specified?
-    raise exception if Settings.airbrake_unspecified?
+    raise exception if should_raise
     nil
   end
 end
