f8bc5d3a0a0f91ea5f3542a5e92306f3174f914b
Devin Gaffney <itsme@devingaffney.com>
2021-03-09 08:28:02 -0600

    CHECK-350 fix AfricaCheck parser (#93)
    * CHECK-350 fix AfricaCheck parser
    
    * Updates to fix test failurs
    
    * Updates to fix test failures

--- lib/claim_review_parsers/africa_check.rb
+++ lib/claim_review_parsers/africa_check.rb
@@ -8,20 +8,30 @@ class AfricaCheck < ClaimReviewParser
   end
 
   def fact_list_path(page = 1)
-    "/latest-reports/page/#{page}/"
+    "/fact-checks?field_article_type_value=reports&field_rated_value=All&field_country_value=All&sort_bef_combine=created_DESC&sort_by=created&sort_order=DESC&page=#{page}"
   end
 
   def url_extraction_search
-    'div#main div.article-content h2 a'
+    'article'
   end
 
   def url_extractor(atag)
-    atag.attributes['href'].value
+    atag.attributes['about'] && atag.attributes['about'].value && hostname+atag.attributes['about'].value
   end
 
-  def claim_result_text_map
+  def claim_review_image_url_from_raw_claim_review(raw_claim_review)
+    raw_claim_review["page"].search("img.attachment-articleMain").first.attributes["src"].value
+  rescue StandardError => e
+    Error.log(e)
+  end
+
+  def claim_review_reviewed_from_raw_claim_review(raw_claim_review)
+    raw_claim_review["page"].search("div.article-details__claims div").text
+  end
+
+  def rating_map
     {
-      'correct' => 1,
+      'correct' => 1.0,
       'mostly-correct' => 0.75,
       'unproven' => 0.5,
       'misleading' => 0.5,
@@ -32,26 +42,36 @@ class AfricaCheck < ClaimReviewParser
     }
   end
 
-  def claim_review_image_url_from_raw_claim_review(raw_claim_review)
-    raw_claim_review["page"].search("img.attachment-articleMain").first.attributes["src"].value
-  rescue StandardError => e
-    Error.log(e)
+  def rating_from_raw_claim_review(raw_claim_review)
+    if raw_claim_review && raw_claim_review["page"]
+      rating_text = raw_claim_review["page"].search('div.article-details__verdict div').first.attributes["class"].value.split(" ").select{|x| x.include?("rating--")}.first.split("--").last
+      [rating_text, rating_map[rating_text]]
+    else
+      [nil, nil]
+    end
+  end
+
+  def extract_news_article_from_ld_json_script_block(ld_json_script_block)
+    ld_json_script_block &&
+    ld_json_script_block["@graph"] &&
+    ld_json_script_block["@graph"].select{|x| x["@type"] == "NewsArticle"}[0]
   end
 
   def parse_raw_claim_review(raw_claim_review)
-    claim_review = extract_ld_json_script_block(raw_claim_review["page"], 0)
+    claim_review = extract_news_article_from_ld_json_script_block(extract_ld_json_script_block(raw_claim_review["page"], 0))
+    claim_review_result, claim_review_result_score = rating_from_raw_claim_review(raw_claim_review)
     if claim_review
       {
         id: raw_claim_review['url'],
-        created_at: Time.parse(claim_review["datePublished"]),
+        created_at: Time.parse(claim_review["datePublished"]||claim_review["dateModified"]),
         author: claim_review["author"]["name"],
         author_link: claim_review["author"]["url"],
-        claim_review_headline: raw_claim_review['page'].search('div#content h1.single-title').text,
+        claim_review_headline: claim_review["headline"],
         claim_review_body: claim_review["description"],
-        claim_review_reviewed: claim_review["claimReviewed"],
-        claim_review_image_url: claim_review_image_url_from_raw_claim_review(raw_claim_review),
-        claim_review_result: claim_review["reviewRating"]["alternateName"].strip,
-        claim_review_result_score: claim_result_score_from_raw_claim_review(claim_review),
+        claim_review_reviewed: claim_review_reviewed_from_raw_claim_review(raw_claim_review),
+        claim_review_image_url: claim_review["image"]["url"],
+        claim_review_result: claim_review_result,
+        claim_review_result_score: claim_review_result_score,
         claim_review_url: raw_claim_review['url'],
         raw_claim_review: claim_review
       }
