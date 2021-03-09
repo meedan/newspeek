f15c3b6323eaa45797c34474f5a8926c382618b3
Devin Gaffney <itsme@devingaffney.com>
2021-03-09 11:03:16 -0600

    CHECK-323 fix checamos

--- lib/claim_review_parsers/afp.rb
+++ lib/claim_review_parsers/afp.rb
@@ -20,6 +20,14 @@ class AFP < ClaimReviewParser
     hostname + atag.attributes['href'].value
   end
 
+  def og_timestamps_from_raw_claim_review(raw_claim_review)
+    raw_claim_review["page"].search("meta").select{|x|
+      x.attributes["property"] && x.attributes["property"].value.include?("_time")
+    }.collect{|x|
+      Time.parse(x.attributes["content"].value) rescue nil
+    }.compact
+  end
+
   def claim_review_headline_from_raw_claim_review_and_claim_review(raw_claim_review, claim_review)
     title = raw_claim_review['page'].search('h1.content-title').text.strip
     title = claim_review["@graph"][0]["name"] if title.empty?
@@ -32,9 +40,10 @@ class AFP < ClaimReviewParser
 
   def parse_raw_claim_review(raw_claim_review)
     claim_review = extract_ld_json_script_block(raw_claim_review["page"], 0)
+    latest_timestamp = [Time.parse(claim_review["@graph"][0]["datePublished"]), og_timestamps_from_raw_claim_review(raw_claim_review)].flatten.sort.last
     {
       id: raw_claim_review['url'],
-      created_at: Time.parse(claim_review["@graph"][0]["datePublished"]),
+      created_at: latest_timestamp,
       author: claim_review["@graph"][0]["author"]["name"],
       author_link: claim_review["@graph"][0]["author"]["url"],
       claim_review_headline: claim_review_headline_from_raw_claim_review_and_claim_review(raw_claim_review, claim_review),
