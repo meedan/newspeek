0dbfbd6805b9cc47acdf8269438c85d5953ffa25
danielevalverde <danierzevalverde@gmail.com>
2021-05-13 21:54:21 -0300

    CHECK-436: Remove commented lines

--- production/bin/configurator.sh
+++ production/bin/configurator.sh
@@ -7,9 +7,6 @@ if [[ -z ${GITHUB_TOKEN+x} || -z ${DEPLOY_ENV+x} || -z ${APP+x} ]]; then
 	exit 1
 fi
 
-# if [ ! -d "configurator" ]; then git clone -q https://${GITHUB_TOKEN}:x-oauth-basic@github.com/meedan/configurator ./configurator; fi
-# d=configurator/check/${DEPLOY_ENV}/${APP}/; for f in $(find $d -type f); do cp "$f" "${f/$d/}"; done
-
 cp config/cookies.json.example config/cookies.json
 cp .env_file.test .env_file
 
