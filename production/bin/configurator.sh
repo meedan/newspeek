98c0a858975db9b7ca11fd7e3a08685c7cb565cb
Martin Peck <martin@meedan.com>
2021-06-02 15:29:09 -0700

    Fetch needs a cookie file.

--- production/bin/configurator.sh
+++ production/bin/configurator.sh
@@ -7,9 +7,10 @@ if [[ -z ${GITHUB_TOKEN+x} || -z ${DEPLOY_ENV+x} || -z ${APP+x} ]]; then
 	exit 1
 fi
 
+cp config/cookies.json.example config/cookies.json
+
 if [[ "$DEPLOY_ENV" != "qa" && "$DEPLOY_ENV" != "live" ]]; then
-	# Only use the test configurations if we're not deploying to QA or Live.
-	cp config/cookies.json.example config/cookies.json
+	# Only use the test configuration if we're not deploying to QA or Live.
 	cp .env_file.test .env_file
 else
 	# The dumb-init process expects an .env_file. Use an empty one for QA and Live.
